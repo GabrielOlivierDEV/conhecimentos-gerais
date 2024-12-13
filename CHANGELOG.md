c8a0727892c93e23d2ad54ee171244ee84be170d
Bieelzy <bieelzyolivier@gmail.com>
2024-12-13 16:29:05 -0300

    MEGA UPDATE 2.0! o código está finalmente bom!

---
+++ CHANGELOG.md
@@ -0,0 +1,34 @@
+# Change Log
+Meio que só fiz uma changelog no update final heh.
+ 
+ 
+## [1.0.0] - 2024-11-28  
+ 
+### Added
+- O jogo inteiro
+ 
+### Changed
+ 
+### Fixed
+ 
+## [1.1.0] - 2024-12-05
+ 
+### Added
+- Musiquinha de fundo
+   
+### Changed
+ 
+### Fixed
+
+## [2.0] - 2024-12-13
+ 
+### Added
+- MAIS 5 PERGUNTAS NOVAS!
+- Maior suporte! Agora é póssivel adicionar mais perguntas facilmente!
+- Quando você errar, tocará o áudio "ERROU" do Faustão!
+
+### Changed
+- COMPLETA REESCRITA DO CÓDIGO!
+ 
+### Fixed
+- Problemas com input.
--- script/conhecimentos gerais.py
+++ script/conhecimentos gerais.py
@@ -1,21 +1,50 @@
-# Don't worry stranger looking at my code, I know it's pretty disgusting, but I'm still learning python, and when I get better, I'll return
-# And I'll make this code less bad
 import pygame
 import os
 pygame.mixer.init()
 base_path = os.path.dirname(__file__)
-file_path = os.path.join(base_path, 'music', 'spookyfunnight.mp3')
-pygame.mixer.music.load(file_path)
-pygame.mixer.music.play(loops=-1)
 
 acertou = 'Resposta correta!'
 errou = 'ERROU!!!!!!!!'
-p1 = 'Sua primeira pergunta é: Qual é o maior planeta do Sistema Solar?\na) Saturno\nb) Júpiter\nc) Marte\nd) Terra\ne) Urano '
-p2 = 'Em que ano aconteceu a Proclamação da República no Brasil?\na) 1879\nb) 1900\nc) 1889\nd) 1857\ne) 1895 '
-p3 = 'Qual é o elemento químico representado pelo símbolo "O"?\na) Oxigênio\nb) Ouro\nc) Osmium\nd) Hidrogênio\ne) Ozônio '
-p4 = 'Quem pintou a obra "A Última Ceia"?\na) Michelangelo\nb) Rafael\nc) Leonardo da Vinci\nd) Van Gogh\ne) Botticelli '
-p5 = 'Qual país é conhecido como a "terra do sol nascente"?\na) China\nb) Coreia do Sul\nc) Japão\nd) Tailândia\ne) Indonésia '
 fim = 'VOCÊ GANHOU!!! Sua alma será poupada... por hoje :3\nRecomeçar? (s/n)'
+
+perguntas = [
+    {"texto":'Sua primeira pergunta é: Qual é o maior planeta do Sistema Solar?\na) Saturno\nb) Júpiter\nc) Marte\nd) Terra\ne) Urano',"resposta":'a'},
+    {"texto":'Em que ano aconteceu a Proclamação da República no Brasil?\na) 1879\nb) 1900\nc) 1889\nd) 1857\ne) 1895',"resposta":'c'},
+    {"texto":'Qual é o elemento químico representado pelo símbolo "O"?\na) Oxigênio\nb) Ouro\nc) Osmium\nd) Hidrogênio\ne) Ozônio',"resposta":'a'},
+    {"texto":'Quem pintou a obra "A Última Ceia"?\na) Michelangelo\nb) Rafael\nc) Leonardo da Vinci\nd) Van Gogh\ne) Botticelli',"resposta":'c'},
+    {"texto":'Qual país é conhecido como a "terra do sol nascente"?\na) China\nb) Coreia do Sul\nc) Japão\nd) Tailândia\ne) Indonésia',"resposta":'c'},
+    {"texto":'Qual é o rio mais longo do mundo?\na) Nilo\nb) Mississippi\nc) Yangtzé\nd) Amazonas\ne) Danúbio',"resposta":'d'},
+    {"texto":'Quem escreveu "Dom Quixote"?\na) William Shakespeare\nb) Miguel de Cervantes\nc) Gabriel García Márquez\nd) Eça de Queirós\ne) Machado de Assis',"resposta":'b'},
+    {"texto":'Qual é a fórmula química da água?\na) H2O2\nb) CO2\nc) H2O\nd) CH4\ne) NaCl',"resposta":'c'},
+    {"texto":'Qual é o maior oceano do planeta?\na) Oceano Atlântico\nb) Oceano Índico\nc) Oceano Ártico\nd) Oceano Antártico\ne) Oceano Pacífico',"resposta":'e'},
+    {"texto":'Em que continente está localizado o Deserto do Saara?\na) América do Norte\nb) Ásia\nc) Austrália\nd) África\ne) Europa',"resposta":'d'}
+
+]
+
+def fazer_perguntas(perguntas):
+    
+     for pergunta in perguntas:
+          print(pergunta["texto"])
+          resposta = input().strip().lower()
+          if resposta == pergunta["resposta"]:
+               print(acertou)
+          else:
+               print(errou)
+               error() 
+               return False
+     return True
+
+def ost():
+    spooky = os.path.join(base_path, 'music', 'spookyfunnight.mp3')
+    pygame.mixer.music.load(spooky)
+    pygame.mixer.music.play(loops=-1)
+
+def error():
+     faustão = os.path.join(base_path, 'music', 'errou.mp3')
+     error_sound = pygame.mixer.Sound(faustão)
+     error_sound.play()
+
+ost()
 
 bemvindo = input(('Bem vindo! você gostaria de jogar um jogo? (s/n) :) ')).strip().lower()
 if bemvindo == 's':
@@ -24,33 +53,9 @@ else:
         print('Você não tem escolha :3',end=' ')
 
 while True:        
-    print(p1)
-    r1 = input()
-
-    if r1 == 'a':
-        print(acertou)
-        print('Você é bom! Vamos para a próxima pergunta!')
-        print(p2)
-
-        r2 = input()
-        if r2 == 'c':
-            print(acertou)
-            print(p3)
-            r3 = input()
-            if r3 == 'a':
-                 print(acertou)
-                 print(p4)
-                 r4 = input()
-                 if r4 == 'c':
-                      print(acertou)
-                      print(p5)
-                      r5 = input()
-                      if r5 == 'c':
-                           print(acertou)
-                           print(fim)
-                           end = input()
-                           if end == 'n':
-                                break
-
-    else:
-        print(errou)
+    venceu = fazer_perguntas(perguntas)
+    if venceu:
+        print(fim)
+        end = input()
+        if end == 'n':
+              break
