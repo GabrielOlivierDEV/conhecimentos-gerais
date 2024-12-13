f4d4af7f66a006e669eeb91b611462b815759c5b
Bieelzy <bieelzyolivier@gmail.com>
2024-12-13 14:18:16 -0300

    FULL REWRITE! however... I'm not done, I still want to make this code less bad after this

--- script/conhecimentos gerais.py
+++ script/conhecimentos gerais.py
@@ -2,66 +2,55 @@
 # And I'll make this code less bad
 import pygame
 import os
-while True:
-    pygame.mixer.init()
-    base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, 'music', 'spookyfunnight.mp3')
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.play(loops=-1)
-    bemvindo = input(('Bem vindo! você gostaria de jogar um jogo? :) '))
-    if bemvindo in ["Sim", "sim", "sim!", "Sim!"]:
+pygame.mixer.init()
+base_path = os.path.dirname(__file__)
+file_path = os.path.join(base_path, 'music', 'spookyfunnight.mp3')
+pygame.mixer.music.load(file_path)
+pygame.mixer.music.play(loops=-1)
+
+acertou = 'Resposta correta!'
+errou = 'ERROU!!!!!!!!'
+p1 = 'Sua primeira pergunta é: Qual é o maior planeta do Sistema Solar?\na) Saturno\nb) Júpiter\nc) Marte\nd) Terra\ne) Urano '
+p2 = 'Em que ano aconteceu a Proclamação da República no Brasil?\na) 1879\nb) 1900\nc) 1889\nd) 1857\ne) 1895 '
+p3 = 'Qual é o elemento químico representado pelo símbolo "O"?\na) Oxigênio\nb) Ouro\nc) Osmium\nd) Hidrogênio\ne) Ozônio '
+p4 = 'Quem pintou a obra "A Última Ceia"?\na) Michelangelo\nb) Rafael\nc) Leonardo da Vinci\nd) Van Gogh\ne) Botticelli '
+p5 = 'Qual país é conhecido como a "terra do sol nascente"?\na) China\nb) Coreia do Sul\nc) Japão\nd) Tailândia\ne) Indonésia '
+fim = 'VOCÊ GANHOU!!! Sua alma será poupada... por hoje :3\nRecomeçar? (s/n)'
+
+bemvindo = input(('Bem vindo! você gostaria de jogar um jogo? (s/n) :) ')).strip().lower()
+if bemvindo == 's':
         print('Vamos nessa!!!',end=' ')
-    else:
+else:
         print('Você não tem escolha :3',end=' ')
-    print('Vamos começar o jogo!')
-    print('Responda escrevendo "a", "b", "c", etc;')
-    p1 = input(('Sua primeira pergunta é: Qual é o maior planeta do Sistema Solar?\na) Saturno\nb) Júpiter\nc) Marte\nd) Terra\ne) Urano '))
-    if p1 == "b":
-        print('Resposta correta!')
-        print ('Você é bom! Vamos para a próxima pergunta!')
-        p2 = input('Em que ano aconteceu a Proclamação da República no Brasil?\na) 1879\nb) 1900\nc) 1889\nd) 1857\ne) 1895 ')
-        if p2 == "c":
-            print('Resposta correta!')
-            p3 = (input('Qual é o elemento químico representado pelo símbolo "O"?\na) Oxigênio\nb) Ouro\nc) Osmium\nd) Hidrogênio\ne) Ozônio '))
-            if p3 == "a":
-                print('Resposta correta!')
-                p4 = (input('Quem pintou a obra "A Última Ceia"?\na) Michelangelo\nb) Rafael\nc) Leonardo da Vinci\nd) Van Gogh\ne) Botticelli '))
-                if p4 == "c":
-                     print('Resposta correta, vamos para a última pergunta!')
-                     p5 = (input('Qual país é conhecido como a "terra do sol nascente"?\na) China\nb) Coreia do Sul\nc) Japão\nd) Tailândia\ne) Indonésia '))
-                     if p5 == "c":
-                          retry = (input('VOCÊ GANHOU!!! Sua alma será poupada... por hoje :3\nRecomeçar? (s/n) '))
-                          if retry == "s":
-                               print('Retomando...')
-                          else:
+
+while True:        
+    print(p1)
+    r1 = input()
+
+    if r1 == 'a':
+        print(acertou)
+        print('Você é bom! Vamos para a próxima pergunta!')
+        print(p2)
+
+        r2 = input()
+        if r2 == 'c':
+            print(acertou)
+            print(p3)
+            r3 = input()
+            if r3 == 'a':
+                 print(acertou)
+                 print(p4)
+                 r4 = input()
+                 if r4 == 'c':
+                      print(acertou)
+                      print(p5)
+                      r5 = input()
+                      if r5 == 'c':
+                           print(acertou)
+                           print(fim)
+                           end = input()
+                           if end == 'n':
                                 break
-                     if p5 != "c":
-                          retry = (input('ERROU!!!!!!!! Tentar novamente? (s/n) '))
-                          if retry == "s":
-                               print('Retomando...')
-                          else:
-                               break
-                if p4 != "c":
-                    retry = (input('ERROU!!!!!!!! Tentar novamente? (s/n) '))
-                    if retry == "s":
-                        print('Retomando...')
-                    else:
-                        break
-            if p3 != "a":
-                retry = (input('ERROU!!!!!!!! Tentar novamente? (s/n) '))
-            if retry == "s":
-                    print('Retomando...')
-            else:
-                break
-        if p2 != "c":
-            retry = (input('ERROU!!!!!!!! Tentar novamente? (s/n) '))
-            if retry == "s":
-                    print('Retomando...')
-            else:
-                break
+
     else:
-        retry = (input('ERROU!!!!!!!! Tentar novamente? (s/n) '))
-        if retry == "s":
-            print('Retomando...')
-        else:
-            break
+        print(errou)
